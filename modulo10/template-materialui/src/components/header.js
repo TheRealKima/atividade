3ff14703b6214318e597bb15c1a8ce38f727463e
Alyson <alysonfranca96@gmail.com>
2024-10-25 13:47:37 -0300

    alteração

--- modulo10/template-materialui/src/components/header.js
+++ modulo10/template-materialui/src/components/header.js
@@ -23,7 +23,7 @@ function Header() {
               <MenuIcon />
               */}
             </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1, backgroundColor: 'red' }}>
               Gerenciamento de Projetos
             </Typography>
           </Toolbar>
